dfd86d474129b25ba40e442d1d033b7b7041bb1c
tutv3 <tutv3@smartosc.com>
2022-01-23 23:27:32 +0700

    setup basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/sidebar';
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import Home from './pages/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app-container">
+        <Sidebar />
+        <div className="main">
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Logo from "../logo";
+import { NavLink } from 'react-router-dom';
+import { AiFillHome } from 'react-icons/ai';
+import { HiOutlineTicket } from 'react-icons/hi';
+import { BsCardChecklist } from 'react-icons/bs'
+import { FiSettings } from 'react-icons/fi'
+import styles from './sidebar.module.css';
+
+const Sidebar = () => {
+  const activeSidebarLink = [styles.active, styles.sidebarLink].join(' ');
+  return (
+    <div className={styles.sidebar}>
+      <div>
+        <Logo />
+        <ul className={styles.sidebarLinks}>
+          <NavLink to="/"
+            className={({ isActive }) => isActive ? activeSidebarLink : styles.sidebarLink}>
+            <AiFillHome color="#1E0D03" size={18} />
+            <span>
+              Trang chủ
+            </span>
+          </NavLink>
+          <NavLink to="/ticket" className={({ isActive }) => isActive ? activeSidebarLink : styles.sidebarLink}>
+            <HiOutlineTicket color="#1E0D03" size={18} /> <span>Quản lý vé</span>
+          </NavLink>
+          <NavLink to="/check" className={({ isActive }) => isActive ? activeSidebarLink : styles.sidebarLink}>
+            <BsCardChecklist color="#1E0D03" size={18} />
+            <span>Đối soát vé</span>
+          </NavLink>
+          <NavLink to="/settings" className={({ isActive }) => isActive ? activeSidebarLink : styles.sidebarLink}>
+            <FiSettings color="#1E0D03" size={18} /> <span>Cài đặt</span>
+          </NavLink>
+        </ul>
+      </div>
+      <div className={styles.sidebarBottom}>
+        <p>
+          Copyright &copy; 2021 Alta Software
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
