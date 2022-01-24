68fc5e1a5f3f48a19e5144954bdc557ab99de5e3
tutv3 <tutv3@smartosc.com>
2022-01-24 16:20:46 +0700

    finish doughnut chart

--- src/components/homeDoughnutChart/index.tsx
+++ src/components/homeDoughnutChart/index.tsx
@@ -1,6 +1,10 @@
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { useState } from "react";
 import { Doughnut } from "react-chartjs-2";
 import styles from "./index.module.css";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { AiOutlineCalendar } from "react-icons/ai";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -29,8 +33,34 @@ const dataEventPackage = {
 };
 
 export function HomeDoughnutChart() {
+  const [profitDate, setProfitDate] = useState(new Date());
+  const [showProfitDate, setShowProfitDate] = useState(false);
   return (
     <div className={styles.row}>
+      <div>
+        <div className={styles.date}>
+          <div className={styles.dateOutput}>
+            <div>
+              {profitDate.toDateString()}
+            </div>
+            <button onClick={() => setShowProfitDate(prevState => !prevState)}>
+              <AiOutlineCalendar size={24} />
+            </button>
+          </div>
+          <div className={styles.calendar}>
+            {
+              showProfitDate && <Calendar
+                onChange={(dateStr: Date) => {
+                  setProfitDate(dateStr);
+                  setShowProfitDate(false);
+                }}
+                value={profitDate}
+                defaultView="month"
+              />
+            }
+          </div>
+        </div>
+      </div>
       <div className={styles.col}>
         <h3 className={styles.colTitle}>Gói gia đình</h3>
         <Doughnut data={dataFamilyPackage} />
@@ -40,8 +70,14 @@ export function HomeDoughnutChart() {
         <Doughnut data={dataEventPackage} />
       </div>
       <div className={styles.col}>
-        <h3 className={styles.colTitle}>Gói sự kiện</h3>
-        {/* <Doughnut data={dataEventPackage} /> */}
+        <div className={styles.specs}>
+          <span className={styles.badge}></span>
+          <span className={styles.badgeText}>Vé đã sử dụng</span>
+        </div>
+        <div className={styles.specs}>
+          <span className={styles.badge} style={{ backgroundColor: 'rgba(255, 138, 72, 1)' }}></span>
+          <span className={styles.badgeText}>Vé chưa sử dụng</span>
+        </div>
       </div>
     </div>
   );
--- src/components/homeLineChart/index.tsx
+++ src/components/homeLineChart/index.tsx
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import ReactApexCharts from "react-apexcharts";
-
+import styles from './index.module.css';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import { AiOutlineCalendar } from "react-icons/ai";
 
 const HomeLineChart = () => {
+  const [profitDate, setProfitDate] = useState(new Date());
+  const [showProfitDate, setShowProfitDate] = useState(false);
   const [chartData] = useState({
     series: [
       {
         name: "Doanh Thu ",
-        data: [0, 178, 158, 240, 42, 210, 220, 170],
+        data: [150, 178, 158, 240, 42, 210, 190],
       },
     ],
     options: {
@@ -22,8 +27,12 @@ const HomeLineChart = () => {
         // curve: 'smooth'
       },
       xaxis: {
-        // type: 'datetime',
-        // categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"],
+        labels: {
+          show: true,
+          // formatter: function (val: number) {
+          //   return val + ' tr';
+          // }
+        }
       },
       tooltip: {
         x: {
@@ -38,15 +47,49 @@ const HomeLineChart = () => {
         }
       }
     },
-  })
+  });
+
   return (
-    <div>
+    <div className={styles.lineChart}>
+      <div className={styles.header}>
+        <h3>Doanh thu</h3>
+        <div className={styles.date}>
+          <div className={styles.dateOutput}>
+            <div>
+              {profitDate.toDateString()}
+            </div>
+            <button onClick={() => setShowProfitDate(prevState => !prevState)}>
+              <AiOutlineCalendar size={24} />
+            </button>
+          </div>
+          <div className={styles.calendar}>
+            {
+              showProfitDate && <Calendar
+                onChange={(dateStr: Date) => {
+                  setProfitDate(dateStr);
+                  setShowProfitDate(false);
+                }}
+                value={profitDate}
+                defaultView="month"
+              />
+            }
+          </div>
+        </div>
+      </div>
       <ReactApexCharts
         options={chartData.options}
         series={chartData.series}
         type="area"
         height={350}
       />
+      <div className={styles.total}>
+        <p>
+          Tổng doanh thu theo tuần
+        </p>
+        <div>
+          <strong>525.145.000 </strong> <span>đồng</span>
+        </div>
+      </div>
     </div>
   );
 };
